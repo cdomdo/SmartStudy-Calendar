0c9575369d607574e4293aaa21540bbd7fad0536
Carlos <dominguezdopazocarlos@gmail.com>
2024-03-13 21:52:04 +0000

    Conflicts resolutions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { LoginregisterComponent } from './components/loginregister/loginregister
 import { ProfileComponent} from "./components/profile/profile.component";
 import {CalendarComponent} from "./components/calendar/calendar.component";
 import {HomeComponent} from "./components/website/home/home.component";
-import {LoginregisterComponent} from "./components/loginregister/loginregister.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
