8dbf7421be5141893ebda3ccd62651e39809b737
m4rc <marcos.sanchez2003@proton.me>
2024-03-17 01:25:04 +0000

    Añadida barra de búsqueda

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -9,11 +9,14 @@ import { NotesService } from '../../services/notes.service';
 })
 export class NotesComponent implements OnInit {
   notes: Note[] = [];
+  filteredNotes: Note[] = [];
   newNote: Note = {title: '', description: ''};
   showForm: boolean = false;
   editingNoteId: string | undefined = undefined;
+  private _searchText: string = '';
 
-  constructor(private notesService: NotesService) { }
+  constructor(private notesService: NotesService) {
+  }
 
   ngOnInit(): void {
     this.loadNotes();
@@ -22,14 +25,30 @@ export class NotesComponent implements OnInit {
   loadNotes(): void {
     this.notesService.getNotes().subscribe(notes => {
       this.notes = notes;
+      this.filterNotes();
     });
+  }
+
+  filterNotes(): void {
+    this.filteredNotes = this.notes.filter(note =>
+      note.title.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+  get searchText(): string {
+    return this._searchText;
+  }
+
+  set searchText(value: string) {
+    this._searchText = value;
+    this.filterNotes();
   }
 
   toggleForm(note?: Note): void {
     this.showForm = !this.showForm;
     if (note) {
       this.editingNoteId = note.id;
-      this.newNote = { ...note };
+      this.newNote = {...note};
     } else {
       this.editingNoteId = undefined;
       this.newNote = {title: '', description: ''};
@@ -38,19 +57,25 @@ export class NotesComponent implements OnInit {
 
   submitNote(): void {
     if (this.editingNoteId) {
+      // Actualizar una nota existente
       this.notesService.modifyNote(this.editingNoteId, this.newNote).then(() => {
         this.resetForm();
+        this.loadNotes(); // Recargar las notas para reflejar los cambios
       });
-    } else if (this.newNote.title && this.newNote.description) {
-      this.notesService.addNote(this.newNote).then(() => {
-        this.resetForm();
-      });
+    } else {
+      // Crear una nueva nota
+      if (this.newNote.title && this.newNote.description) {
+        this.notesService.addNote(this.newNote).then(() => {
+          this.resetForm();
+          this.loadNotes(); // Recargar las notas para incluir la nueva
+        });
+      }
     }
   }
 
   deleteNote(noteId: string): void {
     this.notesService.deleteNote(noteId).then(() => {
-      this.loadNotes();
+      this.loadNotes(); // Recargar las notas después de eliminar
     });
   }
 
@@ -58,6 +83,5 @@ export class NotesComponent implements OnInit {
     this.showForm = false;
     this.newNote = {title: '', description: ''};
     this.editingNoteId = undefined;
-    this.loadNotes();
   }
 }
